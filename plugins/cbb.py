7429caf3b6fdb9b7c131fef7fff454ea24ab4d3a
like5kai <155248547+like5kai@users.noreply.github.com>
2024-11-10 16:26:18 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : @modi_chacha</a>\n○ Channel : @MODI_HUB",
+            text = f"<b>○ Creator : @LM_AdminsBot</a>\n○ Channel : @LinksMachine",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
